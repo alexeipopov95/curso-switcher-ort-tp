20a55747df350f2d56bf043c11d60336b0e9b62d
Alexei <alexei97531@gmail.com>
2022-05-29 16:18:12 -0300

    login view

--- CursoSwitcher/Controllers/LoginController.cs
+++ CursoSwitcher/Controllers/LoginController.cs
@@ -12,10 +12,35 @@ namespace CursoSwitcher.Controllers
             return View();
         }
 
+        private bool UserValidation(String dni, String password)
+        {
+            /*Aqui vamos a hacer las validaciones con relación al usuario si está o no en la base de datos
+             y si corresponde redirigirlo al dashboard.*/
+            bool loged = false;
+            if (!string.IsNullOrEmpty(password) && !string.IsNullOrEmpty(dni))
+            {
+                if (dni == "123" && password == "123") {
+                    loged = true;
+                }
+            }
+            return loged;
+        }
+
+
         [HttpPost]
         public IActionResult Index(String dni, String password)
         {
-            return View();
+            Console.WriteLine("Component: {0} Message: {1} ", dni, password);
+
+            if (UserValidation(dni, password))
+            {
+                TempData["Message"] = "Inicio de sesión correcto."; // Esto estaría bueno mostrarlo en un toast (bootstrap)
+                return RedirectToAction("Index", "Dashboard");
+            } else
+            {
+                TempData["Message"] = "No se encontró al alumno, verifique sus credenciales.";
+                return View();
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
