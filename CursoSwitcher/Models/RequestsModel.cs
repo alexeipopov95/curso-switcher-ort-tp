a38bfcddde2b0d6bafaf52ea9c204d1326647ca7
alexeipopov95 <alexei97531@gmail.com>
2022-06-10 19:59:07 -0300

    dolor de cabeza, pero tcasi terminado el matchmaking

--- CursoSwitcher/Models/RequestsModel.cs
+++ CursoSwitcher/Models/RequestsModel.cs
@@ -7,19 +7,15 @@ namespace CursoSwitcher.Models
     {
         [Key]
         public int Id { get; set; }
-
-        [Display(Name = "Alumno")]
+        [Display(Name = "Solicitante")]
         public int ProfileId { get; set; }
         public ProfileModel? Profile { get; set; }
-
         [Display(Name = "Curso solicitado")]
         public int RequestedCourseId { get; set; }
-        public CoursesModel? RequestedCourse { get; set; }
-
+        public CoursesModel RequestedCourse { get; set; }
         [Display(Name = "Curso ofrecido")]
         public int OfferedCourseId { get; set; }
-        public CoursesModel? OfferedCourse { get; set; }
-
+        public CoursesModel OfferedCourse { get; set; }
         [Display(Name = "Estado")]
         public string status { get; set; }
 
@@ -27,5 +23,39 @@ namespace CursoSwitcher.Models
         {
             this.status = RequestStatusConstantsList.PENDIENTE;
         }
+
+        public class RequestModelMatch
+        {
+            [Display(Name = "Curso solicitado")]
+            public int RequestedCourseId { get; set; }
+            public string? RequestedCourseName { get; set; }
+            [Display(Name = "Curso ofrecido")]
+            public int OfferedCourseId { get; set; }
+            public string? OfferedCourseName { get; set; }
+            [Display(Name = "Id del ofrecedor")]
+            public int OffererId { get; set; }
+            [Display(Name = "Id del solicitante")]
+            public int RequesterId{ get; set; }
+
+            public RequestModelMatch(
+                int requestedCourseId,
+                int offeredCourseId,
+                int offererId,
+                int requesterId,
+                string requestedCourseName,
+                string offeredCourseName
+            )
+            {
+                RequestedCourseId = requestedCourseId;
+                OfferedCourseId = offeredCourseId;
+                OffererId = offererId;
+                RequesterId = requesterId;
+                RequestedCourseName = requestedCourseName;
+                OfferedCourseName = offeredCourseName;
+
+
+            }
+        }
+
     }
 }
