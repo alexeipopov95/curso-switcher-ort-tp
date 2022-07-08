545c8e9ec4f20b79436f4a293736005a4928a561
alexeipopov95 <alexei97531@gmail.com>
2022-07-07 22:27:09 -0300

    added some little improvements

--- CursoSwitcher/Controllers/SignUpController.cs
+++ CursoSwitcher/Controllers/SignUpController.cs
@@ -26,7 +26,7 @@ namespace CursoSwitcher.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Index([Bind("Id,Name,Last_name,Dni,Password,Email,CourseId,CareerId,CampusId")] ProfileModel profileModel)
         {
-            bool userExists = _context.Profiles.Any(e => e.Email.Equals(profileModel.Email));
+            bool userExists = _context.Profiles.Any(e => e.Email.Equals(profileModel.Email) || e.Dni.Equals(profileModel.Dni));
             if (!userExists)
             {
                 _context.Add(profileModel);
@@ -36,7 +36,7 @@ namespace CursoSwitcher.Controllers
             ViewData["CampusId"] = new SelectList(_context.Campus, "Id", "Name");
             ViewData["CareerId"] = new SelectList(_context.Careers, "Id", "Name");
             ViewData["CourseId"] = new SelectList(_context.Courses, "Id", "Name");
-            TempData["Message"] = "Ya existe actualmente una cuenta con ese correo electrónico.";
+            TempData["Message"] = "Ya existe actualmente una cuenta con ese correo electrónico o documento.";
             return View(profileModel);
         }
 
